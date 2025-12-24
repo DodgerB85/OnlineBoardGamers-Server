523cc73b20853aed96ea778f7d4b6ff2bbbf17cf
DodgerB <rogerball85@hotmail.com>
2025-12-24 16:10:12 +0800

    add debug toolbar to dev. Attempt admin speedup

--- WEB/admin.py
+++ WEB/admin.py
@@ -2,10 +2,29 @@ from django.contrib import admin
 
 from .models import WEB_Game#, IND_Tournament
 
+#class WEB_GameAdmin(admin.ModelAdmin):
+#    save_on_top = True
+#    save_as = True
+#    filter_horizontal = ('allPlayers', 'missingPlayers', 'kickedPlayers', 'invitedPlayers')
+    
+@admin.register(WEB_Game)
 class WEB_GameAdmin(admin.ModelAdmin):
-    save_on_top = True
-    save_as = True
-    filter_horizontal = ('allPlayers', 'missingPlayers', 'kickedPlayers', 'invitedPlayers')
+    list_display = ('gameName', 'gameStatus', 'creator', 'latestUpdate')
+    list_filter = ('gameStatus',)
+    search_fields = ('gameName', 'creator__username')
+    
+    # Use autocomplete instead of loading thousands of users into filter_horizontal 
+    autocomplete_fields = [
+        'allPlayers', 'missingPlayers', 'kickedPlayers', 
+        'invitedPlayers', 'winner', 'creator', 'host',
+        'playersWithChatNotification'
+    ]
+    
+    # Prevent loading massive text blobs in the list view
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('creator', 'host').defer('gameData', 'rewindData', 'chatData')
+
+    
 
     #def formfield_for_manytomany(self, db_field, request, **kwargs):
     #    if db_field.name == "allPlayers":
@@ -17,5 +36,5 @@ class WEB_GameAdmin(admin.ModelAdmin):
 #    save_as = True
 #    filter_horizontal = ('startingPlayers', 'nextRoundPlayers')
 
-admin.site.register(WEB_Game, WEB_GameAdmin)
+#admin.site.register(WEB_Game, WEB_GameAdmin)
 #admin.site.register(IND_Tournament, IND_TournamentAdmin)
