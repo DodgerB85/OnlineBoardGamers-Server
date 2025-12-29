ef6bf130819a56f297361352a28dd9533c50358f
Hywel Carver <>
2025-12-29 22:58:47 +0000

    New version of views. Rename game to gameCode, include starting_map, include indication of chat notifications.

---
+++ Lobby/migrations/views/all_games_invited_players/v2.sql
@@ -0,0 +1,46 @@
+CREATE OR REPLACE VIEW Lobby_all_games_invited_players AS (
+  SELECT 'FCM' AS gameCode,
+         fcm_game_id AS id,
+         user_id
+  FROM FCM_fcm_game_invitedPlayers
+  UNION ALL
+  SELECT 'HC' AS gameCode,
+         hc_game_id AS id,
+         user_id
+  FROM HC_hc_game_invitedPlayers
+  UNION ALL
+  SELECT 'BUS' AS gameCode,
+         bus_game_id AS id,
+         user_id
+  FROM Bus_bus_game_invitedPlayers
+  UNION ALL
+  SELECT 'TGZ' AS gameCode,
+         tgz_game_id AS id,
+         user_id
+  FROM TGZ_tgz_game_invitedPlayers
+  UNION ALL
+  SELECT 'CNS' AS gameCode,
+          cns_game_id AS id,
+          user_id
+  FROM CNS_cns_game_invitedPlayers
+  UNION ALL
+  SELECT 'AQY' AS gameCode,
+          aqy_game_id AS id,
+          user_id
+  FROM AQY_aqy_game_invitedPlayers
+  UNION ALL
+  SELECT 'IND' AS gameCode,
+          ind_game_id AS id,
+          user_id
+  FROM IND_ind_game_invitedPlayers
+  UNION ALL
+  SELECT 'KFW' AS gameCode,
+          kfw_game_id AS id,
+          user_id
+  FROM KFW_kfw_game_invitedPlayers
+  UNION ALL
+  SELECT 'WEB' AS gameCode,
+          web_game_id AS id,
+          user_id
+  FROM WEB_web_game_invitedPlayers
+);
