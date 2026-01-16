2c4fdf2b265e9ab4e8535a15832d57ee10eeed0c
Hywel Carver <>
2026-01-16 17:12:10 +0000

    Add uniqueness constraint
    Migration to add uniqueness constraint

---
+++ Lobby/migrations/0059_gameplayer_unique_constraint.py
@@ -0,0 +1,17 @@
+# Generated manually on 2026-01-16
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Lobby', '0058_migrate_cns_games'),
+    ]
+
+    operations = [
+        migrations.AddConstraint(
+            model_name='gameplayer',
+            constraint=models.UniqueConstraint(fields=['game', 'player'], name='unique_game_player'),
+        ),
+    ]
