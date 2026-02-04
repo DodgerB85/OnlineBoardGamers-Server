139db26facb1ca1b7e3a9d035d142e71f10843ca
Hywel Carver <>
2026-02-04 22:29:14 +0000

    Redirect old URLs

--- IND/urls.py
+++ IND/urls.py
@@ -1,15 +1,28 @@
 from django.urls import path
+from django.shortcuts import get_object_or_404, redirect
 
 from . import views
 
 app_name = 'IND'
 
+
+def redirect_old_url(request, original_id):
+    """Redirect old IND game URLs to new unified format"""
+    from Lobby.models import Game
+    game = get_object_or_404(Game, gameCode='IND', original_id=original_id)
+    return redirect('IND:showINDgame', game_id=game.id)
+
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path('IND/', views.showINDgameOLD, name='showINDgame'),
-    
-    path('<int:game_id>/', views.showINDgame, name='showINDgame'),
-    path('<int:game_id>/replay/<int:replayStep>/', views.showINDgame, {'spoilerFree': True}, name='showINDreplayStep'),
+    path('IND/', views.showINDgameOLD, name='showINDgameOLD'),
+
+    # New unified URL format (game_id is the unified Game.id)
+    path('<int:game_id>/show/', views.showINDgame, name='showINDgame'),
+    path('<int:game_id>/show/replay/<int:replayStep>/', views.showINDgame, {'spoilerFree': True}, name='showINDreplayStep'),
+
+    # Old URL format redirect (original_id is the old IND_Game.id)
+    path('<int:original_id>/', redirect_old_url, name='redirect_old_url'),
 
     path('help/', views.INDhelp, name='INDhelp'),
 
