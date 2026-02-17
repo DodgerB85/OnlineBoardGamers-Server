41bd481c2d777ec21deae52e3ec0ca7ef584ec47
DodgerB <rogerball85@hotmail.com>
2026-02-17 09:50:54 +0800

    dismantle FCM game

--- KFW/admin.py
+++ KFW/admin.py
@@ -7,10 +7,5 @@ class KFW_GameAdmin(admin.ModelAdmin):
     save_as = True
     filter_horizontal = ('allPlayers', 'missingPlayers', 'kickedPlayers', 'invitedPlayers')
 
-    #def formfield_for_manytomany(self, db_field, request, **kwargs):
-    #    if db_field.name == "allPlayers":
-    #        kwargs["queryset"] = FCM_Game.objects.filter(creator=request.user)
-    #    return super(FCM_GameAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
-
 
 admin.site.register(KFW_Game, KFW_GameAdmin)
