039405a9443be52093ca843c886942e4e67a93e3
Hywel Carver <>
2026-02-17 16:33:54 +0000

    Fix HC models. Add HC games to admin.

--- Lobby/admin.py
+++ Lobby/admin.py
@@ -13,7 +13,7 @@ from .models import (
     Game,
     GamePlayer,
 )
-from .modelProxies import FCMMiniTournament, TGZMiniTournament, CNSgame, WEBgame, AQYgame, TGZgame, INDgame, Busgame, FCMgame, RNBgame
+from .modelProxies import FCMMiniTournament, TGZMiniTournament, CNSgame, WEBgame, AQYgame, TGZgame, INDgame, Busgame, FCMgame, RNBgame, HCgame
 
 from django.conf import settings
 from django import forms
@@ -487,5 +487,13 @@ class RNBgameAdmin(GameAdmin):
 
     class Meta:
         app_label = "RNB"
+
+@admin.register(HCgame)
+class HCgameAdmin(GameAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(gameCode="HC")
+
+    class Meta:
+        app_label = "HC"
 
 ################### END Register game objects to specific app
