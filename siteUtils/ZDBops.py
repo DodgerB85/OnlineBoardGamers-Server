ad3c022a43b60a8487b39a21cdab81c6b3c09802
DodgerB <rogerball85@hotmail.com>
2026-03-30 21:04:46 +0800

    bus

--- siteUtils/ZDBops.py
+++ siteUtils/ZDBops.py
@@ -17,6 +17,7 @@ from django.http import HttpRequest
 import gzip
 from django.utils.translation import gettext
 from unittest.mock import MagicMock
+import msgpack
 
 from collections import defaultdict
 
@@ -94,15 +95,32 @@ count = 0
 
 TARGET_CODE = "Bus" 
 
-allMainTournaments = Main_Tournament.objects.filter(gameCode=TARGET_CODE)
+allBusFinishedGames = Game.objects.filter(gameCode="BUS", gameStatus="FINISHED")
 
 # print the number of games
-print(f"Number of games: {allMainTournaments.count()}")
+print(f"Number of games: {allBusFinishedGames.count()}")
 
-for tournament in allMainTournaments:
-    tournament.gameCode = "BUS"
-    tournament.save()
-    print(tournament.id)
+errorCount = 0
+for game in allBusFinishedGames:
+    try:
+        # 1. Decode the Base64 string back to binary
+        compressed_bytes = base64.b64decode(game.gameData)
+
+        # 2. Decompress the Gzip binary data
+        # gzip.decompress is the modern, direct way to do this in Python 3
+        msgpacked_data = gzip.decompress(compressed_bytes)
+
+        # 3. Unpack the MessagePack binary into a Python object
+        # raw=False ensures strings are decoded as UTF-8 instead of bytes
+        original_data = msgpack.unpackb(msgpacked_data, raw=False)
+    except Exception as e:
+        errorCount += 1
+
+print(f"Error count: {errorCount}")
+
+
+
+
 
 
 
