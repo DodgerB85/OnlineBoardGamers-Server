dada8d3957a73cecefe12b4b193a9234046da97c
DodgerB <rogerball85@hotmail.com>
2026-05-06 15:52:01 +0800

    make uID for maps editable

--- RNB/models.py
+++ RNB/models.py
@@ -19,7 +19,6 @@ class RNBmap(models.Model):
     hexData = models.JSONField(blank=True, null=True, help_text="JUST the hex data, in format [ [q,r,rotation,terrainID], ...]]")
     uniqueID = models.PositiveIntegerField(
         unique=True,
-        editable=False,
         help_text="Permanent unique ID for this map across all installations."
     )
     # Highscores stored as an array of subArrs [ [userID, score, achievedTS], ...]
