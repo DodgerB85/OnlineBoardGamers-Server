f8f36cbf11cb7269043f1779750f2e7c7e56340d
DodgerB <rogerball85@hotmail.com>
2026-08-20 15:24:59 +0800

    pitts map credits

--- BUS/vueBUS/src/components/FooterBar.vue
+++ BUS/vueBUS/src/components/FooterBar.vue
@@ -1,48 +1,53 @@
 <script setup>
-import * as view from '../js/BUSview.js'
+import * as view from "../js/BUSview.js"
 </script>
 
 <template>
-  <div id="footer">
-    <div class="content">
-      <img :src="view.getImage('bus-box-sm')" class="cover" />
-      <p>
-        Bus is a board game by Jeroen Doumen and Joris Wiersinga, published by <a target='_blank'
-          href='https://www.splottershop.com/'>Splotter Spellen</a>.
-        20th anniversary edition art used with permission, published by <a target='_blank'
-          href='https://capstone-games.com/'>Capstone Games</a>.
-      </p>
-      <p>Online Board Gamers implementation by DodgerB. Permissions Provided. 20th Anniversary Board inspired by Marcin Łysikowski</p>
-      <p>Menu icons from <a href="http://game-icons.net">game-icons.net</a></p>
-    </div>
-  </div>
+	<div id="footer">
+		<div class="content">
+			<img :src="view.getImage('bus-box-sm')" class="cover" />
+			<p>
+				Bus is a board game by Jeroen Doumen and Joris Wiersinga, published by
+				<a target="_blank" href="https://www.splottershop.com/">Splotter Spellen</a>
+				. 20th anniversary edition art used with permission, published by
+				<a target="_blank" href="https://capstone-games.com/">Capstone Games</a>
+				.
+				<br />
+				Pittsburgh map art by Todd Sanders, permissions from Damwise Publications.
+			</p>
+			<p>Online Board Gamers implementation by DodgerB. Permissions Provided. 20th Anniversary Board inspired by Marcin Łysikowski</p>
+			<p>
+				Menu icons from
+				<a href="http://game-icons.net">game-icons.net</a>
+			</p>
+		</div>
+	</div>
 </template>
 
 <style scoped>
 #footer {
-  background-color: #333;
-  color: #eee;
-  margin: 0;
-  min-height: 125px;
-  height: fit-content;
-  padding: 10px;
-  min-width: 1000px;
-
+	background-color: #333;
+	color: #eee;
+	margin: 0;
+	min-height: 125px;
+	height: fit-content;
+	padding: 10px;
+	min-width: 1000px;
 }
 
 #footer .content {
-  width: 95%;
-  margin: auto;
-  text-align: left;
+	width: 95%;
+	margin: auto;
+	text-align: left;
 }
 
 #footer img.cover {
-  float: left;
-  height: 100px;
-  margin-right: 10px;
+	float: left;
+	height: 100px;
+	margin-right: 10px;
 }
 
 #footer a {
-  color: #d4eafd;
+	color: #d4eafd;
 }
 </style>
